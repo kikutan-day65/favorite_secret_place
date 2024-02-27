2d4fd852ea22ea4e49328d28b5ca4bac48a13abc
Haruto Mori <baystarsman@outlook.jp>
2024-02-27 19:27:45 +0900

    feat: add CustomUserModel

--- fav_sec_place/apps/core/models.py
+++ fav_sec_place/apps/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+from uuid import uuid4
+
+
+class CustomUserModel(BaseUserManager):
+    def create_user(self, username, email, password):
+        if not email:
+            raise ValueError("Users must have an email address")
+        new_user = self.model(username=username, email=self.normalize_email(email))
+        new_user.set_password(password)
+        new_user.save()
+        return new_user
