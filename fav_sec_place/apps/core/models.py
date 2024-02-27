bd402f2aa995878f3ad5edfe11482fc96b525333
Haruto Mori <baystarsman@outlook.jp>
2024-02-27 20:26:02 +0900

    feat: add photo model

--- fav_sec_place/apps/core/models.py
+++ fav_sec_place/apps/core/models.py
@@ -39,3 +39,18 @@ class Place(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Photo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    photo_path = models.CharField(max_length=255, unique=True, null=False)
+    place_id = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        related_name='photos'
+    )
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='photos'
+    )
