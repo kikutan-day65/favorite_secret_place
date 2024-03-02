cf677594ea15501890ab5d6a48781319aae29aa1
Haruto Mori <baystarsman@outlook.jp>
2024-03-02 23:24:03 +0900

    feat: enable post method in search box

--- fav_sec_place/apps/core/views.py
+++ fav_sec_place/apps/core/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
+    if request.method == 'POST':
+        query = request.POST.get('query')
+        print(query)
     return render(request, 'core/home.html')
