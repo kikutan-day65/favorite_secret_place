ee24390f4131e7e8de539b1606dd2d96cc983391
Haruto Mori <baystarsman@outlook.jp>
2024-03-04 22:59:48 +0900

    feat: add user registration form

---
+++ fav_sec_place/apps/accounts/forms.py
@@ -0,0 +1,45 @@
+
+from django import forms
+from apps.core.models import User
+
+from collections import OrderedDict
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    password_confirm = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password',
+            'email'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields = OrderedDict([
+            ('username', self.fields['username']),
+            ('email', self.fields['email']),
+            ('password', self.fields['password']),
+            ('password_confirm', self.fields['password_confirm']),
+        ])
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords does not match")
+        
+        return cleaned_data
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+ 
