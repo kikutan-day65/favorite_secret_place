9dae8ba6b8ba8a57915681916a61b68821ac31a3
Haruto Mori <baystarsman@outlook.jp>
2024-03-04 23:01:32 +0900

    feat: enable to render the user registration form

--- fav_sec_place/apps/accounts/views.py
+++ fav_sec_place/apps/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import UserRegistrationForm
 
-# Create your views here.
+
+def login_register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'accounts/login_register.html', {'form': form})
