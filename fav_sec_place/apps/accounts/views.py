e8b50052d8af4cd8d261ab3655e0424b9f4a4503
Haruto Mori <baystarsman@outlook.jp>
2024-03-06 19:35:34 +0900

    feat: add view function for user login

--- fav_sec_place/apps/accounts/views.py
+++ fav_sec_place/apps/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 
 
 def register(request):
@@ -11,3 +11,13 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
