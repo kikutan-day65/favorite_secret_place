fbed4c069ca086e1f65c8644499ae3f3fc48a16e
Haruto Mori <baystarsman@outlook.jp>
2024-03-06 19:35:59 +0900

    feat: add user login form

--- fav_sec_place/apps/accounts/forms.py
+++ fav_sec_place/apps/accounts/forms.py
@@ -1,5 +1,6 @@
-
 from django import forms
+from django.contrib.auth import authenticate
+
 from apps.core.models import User
 
 from collections import OrderedDict
@@ -42,4 +43,21 @@ class UserRegistrationForm(forms.ModelForm):
         if commit:
             user.save()
         return user
+    
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(label='Username')
+    password = forms.CharField(widget=forms.PasswordInput(), label='Password')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is None:
+                raise forms.ValidationError("Invalid username or password")
+        return cleaned_data
+
  
